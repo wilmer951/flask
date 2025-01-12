53826ad0cf8c54cbd6da246f4dae35d620d02534
wilmer951 <wilmer-951@hotmail.com>
2025-01-12 11:49:13 -0500

    funcion descargar nuevo archivo

--- .venv/app.py
+++ .venv/app.py
@@ -1,12 +1,15 @@
 import os
 import pandas as pd
-from flask import Flask, request, render_template, flash, redirect, url_for
+from openpyxl import load_workbook
+
+
+from flask import Flask, request, render_template, flash, redirect, url_for,send_from_directory
 
 app = Flask(__name__)
 app.jinja_env.cache = {}
 
 # Configuración para la carpeta de carga
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 app.config['ALLOWED_EXTENSIONS'] = {'xls', 'xlsx'}
 app.secret_key = 'your_secret_key'  # Para mensajes flash
 
@@ -36,7 +39,7 @@ def integration():
 @app.route('/uploadfile', methods=['GET', 'POST'])
 def uploadfile():
 
-
+    status=0
  
     if request.method == 'POST':
     
@@ -57,20 +60,23 @@ def uploadfile():
                 # Si el archivo es válido
         if file and allowed_file(file.filename):
                     # Guardar el archivo en la carpeta de uploads
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], "data.xmls")
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], "data.xlsx")
             file.save(filepath)
             flash(f'Archivo "{file.filename}" cargado exitosamente!')
-            return redirect(request.url)
-        
+
+            status=1
+            return render_template("integration.html", stcarga=status)
+          
 
         else:
             flash('Solo se permite archivos, xls, xlsx')
-            return redirect(request.url)
+            status=0
+            return render_template("integration.html", stcarga=status)
         
 
+    
 
 
-    return render_template("integration.html")
 
 
 
@@ -79,28 +85,106 @@ def uploadfile():
 @app.route('/analystfile', methods=['GET', 'POST'])
 def analystfile():
 
-
+    status=0
  
     if request.method == 'POST':
+
+        data_file=os.path.join(app.config['UPLOAD_FOLDER'], "data.xlsx")
     
-        print("analisis realizado")
+        # Abrir el archivo de Excel con openpyxl
+        wb = load_workbook(data_file)
+        ws = wb.active
+
+        # Obtener las filas visibles (comprobando si están ocultas por el filtro)
+        visible_rows = []
+        for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+            if not ws.row_dimensions[row[0].row].hidden:  # Si la fila no está oculta
+                visible_rows.append([cell.value for cell in row])
+
+        # Crear un DataFrame con las filas visibles
+        columns = [cell.value for cell in ws[1]]
+        df_visible = pd.DataFrame(visible_rows, columns=columns)
+
+        # Asignar las columnas 'HOMBRE' y 'MUJER'
+        df_visible['HOMBRE'] = ''
+        df_visible['MUJER'] = ''
+
+        df_visible['CONTRIBUTIVO'] = ''
+        df_visible['SUBSIDIADO'] = ''
+
+        df_visible['CATEGORIA'] = ''
+        df_visible['SINCATEGORIA'] = ''
 
 
-        data_clientes = os.path.join(app.config['UPLOAD_FOLDER'], "data.xmls")
+        # Asegurarse de que no haya espacios en blanco ni diferencias de mayúsculas/minúsculas
+        df_visible['VICTIMA DE CONFLICTO'] = df_visible['VICTIMA DE CONFLICTO'].str.strip(
+        ).str.upper()
+        df_visible['MIGRANTE'] = df_visible['MIGRANTE'].str.strip().str.upper()
+        df_visible['CARCELARIO'] = df_visible['CARCELARIO'].str.strip().str.upper()
+        df_visible['EN ESTADO DE GESTACION'] = df_visible['EN ESTADO DE GESTACION'].str.strip(
+        ).str.upper()
+        df_visible['OTRO'] = df_visible['OTRO'].str.strip().str.upper()
+        df_visible['DESPLAZADO'] = df_visible['DESPLAZADO'].str.strip().str.upper()
 
 
-        df = pd.read_excel(data_clientes)
+        # Asignar la edad a 'hombres' si el sexo es 'M' (masculino), o a 'mujeres' si el sexo es 'F' (femenino)
+        df_visible.loc[df_visible['SEXO'] == 'M',
+                    'HOMBRE'] = df_visible['EDAD'].astype(int)
+        df_visible.loc[df_visible['SEXO'] == 'F',
+                    'MUJER'] = df_visible['EDAD'].astype(int)
+
+        df_visible.loc[df_visible['REGIMEN'] == 'Contributivo', 'CONTRIBUTIVO'] = 'X'
+        df_visible.loc[df_visible['REGIMEN'] == 'Subsidiado', 'SUBSIDIADO'] = 'X'
+
+        for index, row in df_visible.iterrows():
+            if (row['VICTIMA DE CONFLICTO'] == 'SI' or
+                row['MIGRANTE'] == 'SI' or
+                row['CARCELARIO'] == 'SI' or
+                row['EN ESTADO DE GESTACION'] == 'SI' or
+                row['OTRO'] == 'SI' or
+                    row['DESPLAZADO'] == 'SI'):
+                df_visible.at[index, 'CATEGORIA'] = 'X'
+
+            else:
+                df_visible.at[index, 'SINCATEGORIA'] = 'X'
 
 
-        print(df)
+        # Obtener la fecha y hora actual en formato 'YYYYMMDD_HHMMSS'
 
 
-        return render_template("integration.html")
+        # Guardar el DataFrame modificado en un nuevo archivo de Excel
+    
+ 
+        output_file = os.path.join(app.config['UPLOAD_FOLDER'], "datamodficado.xlsx")
+        
+
+
+        df_visible.to_excel(output_file, index=False)
+        
+        status=1
+    
+
+
+       
+
+        return render_template("integration.html",stdf=status)
 
 
 
+@app.route('/downloaderfile', methods=['POST'])
+def downloaderfile():
+    
+    if request.method == 'POST':
+        file_name = "datamodficado.xlsx"
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
 
-
+        # Verificar si el archivo existe
+        if os.path.exists(file_path):
+            return send_from_directory(app.config['UPLOAD_FOLDER'], file_name, as_attachment=True)
+        else:
+            return f"Archivo no encontrado: {file_path}", 404
+            
+        
 
 
 if __name__ == "__main__":
